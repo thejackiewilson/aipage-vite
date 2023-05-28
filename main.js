9df507e59b5a2c5addc5a6ebbba819f10f187ba0
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2023-05-28 10:44:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.getElementById('search-input');
   const cards = document.querySelectorAll('.link-card');
 
+  let timeout = null;
+
   searchInput.addEventListener('input', (event) => {
-    const searchTerm = event.target.value.toLowerCase();
-  
-    cards.forEach((card) => {
-      const title = card.querySelector('.card-title').textContent.toLowerCase();
-      const description = card.querySelector('.card-text').textContent.toLowerCase();
-  
-      if (title.includes(searchTerm) || description.includes(searchTerm)) {
-        card.style.display = 'block';
-      } else {
-        card.style.display = 'none';
-      }
-    });
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      const searchTerm = event.target.value.toLowerCase();
+
+      cards.forEach((card) => {
+        const title = card.querySelector('.card-title').textContent.toLowerCase();
+        const description = card.querySelector('.card-text').textContent.toLowerCase();
+
+        card.style.display = (title.includes(searchTerm) || description.includes(searchTerm)) ? 'block' : 'none';
+      });
+    }, 300); // delay of 300ms
   });
-  
 
   // Dark mode toggle
   const darkModeToggle = document.getElementById('dark-mode-toggle');
@@ -26,20 +27,28 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.toggle('dark-mode');
     const isDarkMode = document.body.classList.contains('dark-mode');
     localStorage.setItem('dark-mode', isDarkMode);
-  
-    const cardTitles = document.querySelectorAll('.card-title');
-    const cardTexts = document.querySelectorAll('.card-text');
-    const categoryHeaders = document.querySelectorAll('.category-header');
-  
-    if (isDarkMode) {
-      cardTitles.forEach((title) => title.style.color = '#fff');
-      cardTexts.forEach((text) => text.style.color = '#fff');
-      categoryHeaders.forEach((header) => header.style.borderColor = '#fff');
-    } else {
-      cardTitles.forEach((title) => title.style.color = '#2e3c5a');
-      cardTexts.forEach((text) => text.style.color = '#333');
-      categoryHeaders.forEach((header) => header.style.borderColor = '#2e3c5a');
-    }
+  });
+
+  // Apply dark mode if it was set before
+  if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-mode');
+  }
+});
+
+// This function copies the given text to the clipboard
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Could not copy text: ', err);
+  });
+}
+
+// Add an event listener to all 'copy-prompt-btn' buttons
+document.querySelectorAll('.copy-prompt-btn').forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    // The text to be copied is the content of the preceding 'card-text' element
+    let text = event.target.previousElementSibling.innerText;
+    copyToClipboard(text);
   });
 });
-  
